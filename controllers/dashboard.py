e466cc6fc3d9a236ad75af3efaa0179f2ae5f7ca
Radu Ioan Fericean <radu@fericean.ro>
2017-03-10 22:34:11 +0200

    same domain

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -3,7 +3,7 @@ import accurate
 @auth.requires(auth.has_membership(group_id='admin'))
 def status():
     session.forget(response)
-    return accurate.call("Responder.Status", dict(showbytes=True))
+    return accurate.call("Responder.Status", dict(ShowBytes=True))
 
 @auth.requires(auth.has_membership(group_id='admin'))
 def index():
