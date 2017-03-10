68099c9c917d71a33a540040235f87861b486a6d
Radu Ioan Fericean <radu@fericean.ro>
2017-03-10 22:19:11 +0200

    handle bytes

--- controllers/dashboard.py
+++ controllers/dashboard.py
@@ -3,7 +3,7 @@ import accurate
 @auth.requires(auth.has_membership(group_id='admin'))
 def status():
     session.forget(response)
-    return accurate.call("Responder.Status")
+    return accurate.call("Responder.Status", dict(showbytes=True))
 
 @auth.requires(auth.has_membership(group_id='admin'))
 def index():
